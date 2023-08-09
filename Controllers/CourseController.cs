226b0c3c89eefab94c01fd8576dae1447e13bace
celil çavuş <celilcavus13@hotmail.com>
2023-08-10 00:35:47 +0300

    BtkAkademi App tamamlandi

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,38 @@
+﻿using BtkAkademi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BtkAkademi.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var models = Repository.Applications.ToList();
+            return View(models);
+        }
+        public IActionResult Apply()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Apply([FromForm] Candidate model)
+        {
+            if (Repository.Applications.Any(m => m.Email!.Equals(model.Email)))
+            {
+                ModelState.AddModelError("", "There is already applications");
+            }
+
+            if (ModelState.IsValid)
+            {
+                Repository.Add(model);
+                return View("FeedBack", model);
+            }
+            return View();
+        }
+
+
+    }
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,25 @@
+﻿namespace BtkAkademi.Models
+{
+    public static class Repository
+    {
+        private static List<Candidate> applications = new();
+        public static IEnumerable<Candidate> Applications => applications;
+
+
+        
+        public static void Add(Candidate candidate)
+        {
+            applications.Add(candidate);
+        }
+
+        public static void Remove(Candidate candidate)
+        {
+            applications.Remove(candidate);
+        }
+        
+        public static void Update(Candidate candidate)
+        {
+            
+        }
+    }
+}
